de623f97e411c6c100346f35d00114987906e2b3
hrldspnz <haroldespinoza823@gmail.com>
2021-11-06 16:39:38 -0600

    API

---
+++ APINutriTec/Controllers/UsuarioController.cs
@@ -0,0 +1,61 @@
+﻿using APINutriTec.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APINutriTec.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : Controller
+    {
+        
+        String cadenaConexion;
+        public UsuarioController()
+        {
+            cadenaConexion = "Server=tcp:bases-tec.database.windows.net,1433;Initial Catalog=NutriTECBD;Persist Security Info=False;User ID=hadmin;Password=marioNeta1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        }
+
+        [HttpGet]
+        // Get: api/usuario
+
+        public List<Usuario> Get()
+        {
+            SqlDataReader reader = null;
+            SqlConnection myConnection = new SqlConnection();
+
+            myConnection.ConnectionString = cadenaConexion;
+
+            SqlCommand sqlCmd = new SqlCommand();
+
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "SELECT * FROM USUARIO";
+            sqlCmd.Connection = myConnection;
+
+            myConnection.Open();
+
+            reader = sqlCmd.ExecuteReader();
+
+            List<Usuario> nutricionistas = new List<Usuario>();
+
+            while (reader.Read())
+            {
+                Usuario nut = new Usuario();
+
+                nut.codigo_nutricionista = reader.GetValue(7).ToString();
+                nut.nombre1 = reader.GetValue(0).ToString();
+                nut.apellido1 = reader.GetValue(2).ToString();
+
+                nutricionistas.Add(nut);
+
+            }
+            myConnection.Close();
+
+            return nutricionistas;
+        }
+    }
+}
---
+++ APINutriTec/Model/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APINutriTec.Model
+{
+    public class Usuario
+    {
+        public string nombre1 { get; set; }
+        public string nombre2 { get; set; }
+        public string apellido1 { get; set; }
+        public string apellido2 { get; set; }
+        public int cedula { get; set; }
+        public DateTime fecha_de_nacimiento { get; set; }
+        public int edad { get; set; }
+        public string codigo_nutricionista { get; set; }
+        public string pass { get; set; }
+        public string email { get; set; }
+        public int numero_tarjeta { get; set; }
+        public string tipo_cobro { get; set; }
+        public string rol { get; set; }
+        public string direccion { get; set; }
+        public string foto { get; set; }
+    }
+}
