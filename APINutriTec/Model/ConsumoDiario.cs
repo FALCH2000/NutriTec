6589ef36c3ca0d20c4a6bede7b73e3b13a2dc4c5
hrldspnz <haroldespinoza823@gmail.com>
2021-11-08 21:57:57 -0600

    metodos api

---
+++ APINutriTec/Model/ConsumoDiario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APINutriTec.Model
+{
+    public class ConsumoDiario
+    {
+		public string fecha { get; set; }
+		public string almuerzo { get; set; }
+		public string cena { get; set; }
+		public string desayuno { get; set; }
+		public string merienda_manana { get; set; }
+		public string merienda_tarde { get; set; }
+		public int consumo_calorias { get; set; }
+		public int cedula_paciente { get; set; }
+	}
+}
---
+++ APINutriTec/Model/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APINutriTec.Model
+{
+    public class Producto
+    {
+        public string codigo_de_barras { get; set; }
+        public string descripcion { get; set; }
+        public int tamano_porcion { get; set; }
+        public int energia { get; set; }
+        public int grasa { get; set; }
+        public int sodio { get; set; }
+        public int carbohidrato { get; set; }
+        public int proteina { get; set; }
+        public int calcio { get; set; }
+        public string estado { get; set; }
+        public int hierro { get; set; }
+    }
+}
