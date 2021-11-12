a5c84078b7ba2ad0d14e9830b3e203063feec71a
Carmen Valentina Araya <49073400+varaya64@users.noreply.github.com>
2021-11-12 12:44:46 -0600

    Gestion de Productos

---
+++ nutri-view/src/app/services/productos.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  public url: string;
+
+  constructor(public _http: HttpClient
+    ){
+      this.url = "https://nutritecapi.azurewebsites.net/"
+  }
+
+
+  updateProducto(dataProducto: Object, codigo: string, estado: string): Observable<any>{
+    console.log(this.url+'api/producto/aprovacion/'+codigo+'/'+estado, dataProducto)
+    return this._http.put(this.url+'api/producto/aprovacion/'+codigo+'/'+estado, dataProducto);
+  }
+
+  getProductos(): Observable<any> {
+    console.log(this.url + 'api/producto')
+    return this._http.get(this.url + 'api/producto');
+  }
+
+}
